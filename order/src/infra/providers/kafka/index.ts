b6171cc08b79e4024416f0cb3b4ca4315a6674da
Alexandre <alexandre01393@gmail.com>
2023-04-17 14:00:21 -0300

    fix: create kafka username and password env

--- order/src/infra/providers/kafka/index.ts
+++ order/src/infra/providers/kafka/index.ts
@@ -4,8 +4,8 @@ export const kafka = new Kafka({
   brokers: ["dynamic-muskox-12299-us1-kafka.upstash.io:9092"],
   sasl: {
     mechanism: "scram-sha-256",
-    username: "ZHluYW1pYy1tdXNrb3gtMTIyOTkklZitU_o3AIiHADixh8mDl-tKXmpferzor7I",
-    password: "d57af3ed6c314d4aa11e60ab9ae46818",
+    username: process.env.KAFKA_USERNAME as string,
+    password: process.env.KAFKA_PASSWORD as string,
   },
   ssl: true,
 });
